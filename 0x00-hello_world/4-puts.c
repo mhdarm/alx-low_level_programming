ba72432b4b95f6aa20ceea316440fdc255c0731f
mhdarm <mhdarmlry@yahoo.com>
2023-02-16 04:07:29 -0800

    puts c file 0.02

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
+vi 4-puts.c
+#include <stdio.h>
 /**
- * main - Entry point
- * return: Always 0 (Success)
- */
+*main - Entry point
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-	puts("\*Programing is like building a multilingual puzzle");
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
+
