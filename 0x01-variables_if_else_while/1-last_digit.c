c3f2df189a27f2aadea707b3d0369b2d218fe7e2
mhdarm <mhdarmlry@yahoo.com>
2023-02-17 15:06:22 -0800

    0.02

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
 i/*
  * File: 1-last_digit.c
- * Auth: Mohammed Bin Dawuda Arimiyao
+ * Auth: Mohammed Arimiyao
  */
 
 #include <stdlib.h>
@@ -18,7 +18,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 4;
+	n = rand() - RAND_MAX / 2;
 
 	if ((n % 10) > 5)
 	{
