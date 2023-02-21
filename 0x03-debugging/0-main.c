fbeb261fee0f843b5229226dec2932991c2baa59
mhdarm <mhdarmlry@yahoo.com>
2023-02-21 08:12:32 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - this code tests the function that print if an integer is + or -
+ * return: 0 (success)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
