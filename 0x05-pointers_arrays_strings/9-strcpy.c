c7fbb9e0427f771c4245ad008867b3dac7e54ee0
mhdarm <mhdarmlry@yahoo.com>
2023-02-27 06:37:46 -0800

    0.02

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 
 /**
 * char *_strcpy - a function that copies the string pointed to by src
-* @dest: copy to 
+* @dest: copy to
 * @src: copy from
 * Return: string
 */
@@ -11,6 +11,7 @@ char *_strcpy(char *dest, char *src)
 {
 	int l = 0;
 	int x = 0;
+
 	while (*(src + l) != '\0')
 	{
 		l++;
