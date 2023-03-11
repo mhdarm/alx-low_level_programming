286f72cda922ceab273df8e6fe1bc6cd3db3cc48
mhdarm <mhdarmlry@yahoo.com>
2023-03-11 15:50:09 -0800

    0.02

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
 /**
  * check_num - check - string there are digit
  * @str: array str
  *
  * Return: Always 0 (Success)
  */
-
 int check_num(char *str)
 {
 	/*Declaring variables*/
